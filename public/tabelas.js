12d192acd6b848e78445927deaf5d4a029800d48
Renan Rodrigues <renansil.rodrigues@gmail.com>
2024-09-04 17:05:04 -0300

    API p/ tabela

--- public/tabelas.js
+++ public/tabelas.js
@@ -1,11 +1,43 @@
 async function fetchProdutos() {
+
     try {
+
         const response = await fetch('/api/produtos');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
         const produtos = await response.json();
 
-        const jsonContainer = document.getElementById('barra_tabeladecadastro');
+        const tbody = document.querySelector('#tabeladecadastro tbody');
 
-        jsonContainer.textContent = JSON.stringify(produtos);
+        tbody.innerHTML = '';
+
+        produtos.forEach(produto => {
+            // Cria uma nova linha
+            const row = document.createElement('tr');
+
+            const cellId = document.createElement('td');
+            cellId.textContent = produto.id;
+            row.appendChild(cellId);
+
+            const cellTipo = document.createElement('td');
+            cellTipo.textContent = produto.tipo;
+            row.appendChild(cellTipo);
+
+            const cellDescricao = document.createElement('td');
+            cellDescricao.textContent = produto.descricao;
+            row.appendChild(cellDescricao);
+
+            const cellValor = document.createElement('td');
+            cellValor.textContent = produto.valor;
+            row.appendChild(cellValor);
+
+            tbody.appendChild(row);
+        });
+
+        /*const jsonContainer = document.getElementById('barra_tabeladecadastro');
+
+        jsonContainer.textContent = JSON.stringify(produtos);*/
 
 
     } catch (err) {
