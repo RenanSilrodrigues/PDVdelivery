b0e17e0a6b060596c9f399cc6b0a103b37c64116
Renan Rodrigues <renansil.rodrigues@gmail.com>
2024-09-09 16:55:44 -0300

    Nova rota cliente

--- public/main.js
+++ public/main.js
@@ -5,21 +5,8 @@ const abrirJanelaPedido = document.querySelector('.botao_novopedido');
 
 abrirJanelaCadastro.addEventListener('click', () => {
     window.open('/cadastro','_blank','width=1000,height=800,left=450,top=100');
-})
-
-abrirJanelaCliente.addEventListener('click', () => {
-    window.open('cadastro_produto.html','_blank','width=1000,height=800,left=450,top=100');
-})
-
-abrirJanelaSangria.addEventListener('click', () => {
-    window.open('cadastro_produto.html','_blank','width=1000,height=800,left=450,top=100');
-})
-
-abrirJanelaPedido.addEventListener('click', () => {
-    window.open('cadastro_produto.html','_blank','width=1000,height=800,left=450,top=100');
-})
-
-document.addEventListener('DOMContentLoaded', function() {
-    fetchProdutos();
 });
 
+abrirJanelaCliente.addEventListener('click', () => {
+    window.open('/cliente','_blank','width=1000,height=800,left=450,top=100');
+});
