0e2501404e3ba252d1d8a7aa1a948764a7307b08
Renan Rodrigues <renansil.rodrigues@gmail.com>
2024-11-09 23:56:12 -0300

    Ajuste

--- src/components/BannerMenu.vue
+++ src/components/BannerMenu.vue
@@ -5,7 +5,7 @@ import { useRouter } from 'vue-router';
 const router = useRouter();
 
 const logout = () => {
-  localStorage.removeItem('token'); // Remover o token de autenticação
+  localStorage.removeItem('token');
   router.push({ name: 'home' });
 };
 
